16feb84a533035662538414399ec5229aaa504bb
JLarky <jlarky@gmail.com>
2023-01-08 11:50:26 -0700

    add qgp

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
